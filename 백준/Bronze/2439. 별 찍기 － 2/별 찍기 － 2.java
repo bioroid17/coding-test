e2762b7ac71128e30883f506ac31941ea3fbd639
bioroid17 <trace1997@ajou.ac.kr>
2025-06-15 23:43:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 432 ms, Memory: 21920 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 1; i <= t; i++) {
+			for(int j = 1; j <= t - i; j++) {
+				System.out.printf(" ");
+			}
+			for(int k = t - i; k < t; k++) {
+				System.out.printf("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
