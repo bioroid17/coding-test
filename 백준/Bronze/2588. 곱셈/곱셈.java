b0a88a966bc7ac7227d71038d027e6ea9cd47e5f
bioroid17 <trace1997@ajou.ac.kr>
2025-06-15 23:47:42 +0900

    [Bronze III] Title: 곱셈, Time: 212 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int result = a * b;
+
+		while (b != 0) {
+			System.out.println(a * (b % 10));
+			b /= 10;
+		}
+		System.out.println(result);
+		sc.close();
+	}
+}
